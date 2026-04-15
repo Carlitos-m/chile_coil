22809072e3060e3b3618d1253b18c6532fc18ea3
carlosmanuelmtz <a01749154@tec.mx>
2026-04-14 23:40:01 -0600

    primer commit limpio

---
+++ app.py
@@ -0,0 +1,17 @@
+import polars as pl
+import numpy as np
+import streamlit as st
+df=pl.read_parquet('/notebooks/datos_correctos')
+# Notas del flujo
+# 1. Entrada de datos: funcion que recolecte datos del usuario mediante inputs
+# 2. Mapeo nutricional:(Consultar)
+# 3. Estandarizacion por energia: consultar una base de datos
+# 4. Calculo de componenetes: Aplicar las formulas de puntuacion (funciones) a los 13 componentes
+# 5. # Suma Total: La puntuación final es la suma de los 13 resultados individuales ($0 - 100$ puntos).
+# Mostrar info básica
+st.write("Vista previa:")
+
+
+
+
+#%%
---
+++ src/funciones.py
@@ -0,0 +1,5 @@
+import polars as pl
+import numpy as np
+import math
+import streamlit as st
+def entrada_datos():
